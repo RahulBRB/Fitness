43b13a148d8d6c63f1111bd6edf69e9144876ff4
R B <86495244+ryzenRB@users.noreply.github.com>
2022-09-05 13:54:06 +0530

    Create fitness.c

---
+++ fitness.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int t;
+    scanf("%d", &t);
+    
+    int x;
+    while(t--){
+        
+        scanf("%d", &x);
+        printf("%d \n", x*2*5);
+    }
+	return 0;
+}
+
